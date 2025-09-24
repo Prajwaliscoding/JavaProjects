d28e5539ba9df1fe0242c12a2c3f96d24cac4f71
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2025-09-23 22:55:12 -0500

    Added rectangle sorter project, used constructor

---
+++ src/javaprojects/Rectangle.java
@@ -0,0 +1,30 @@
+package javaprojects;
+
+// Prajwal Khatiwada
+public class Rectangle {
+    // Store rectangle's length
+    private double lengthValue;
+    // Store rectangle's width
+    private double widthValue;
+
+    // Constructor: initialize length and width when object is created
+    public Rectangle(double lengthValue, double widthValue) {
+        this.lengthValue = lengthValue;
+        this.widthValue = widthValue;
+    }
+
+    // Getter method: returns length
+    public double getLength() {
+        return lengthValue;
+    }
+
+    // Getter method: returns width
+    public double getWidth() {
+        return widthValue;
+    }
+
+    // Calculate and return area (length × width)
+    public double getArea() {
+        return lengthValue * widthValue;
+    }
+}
